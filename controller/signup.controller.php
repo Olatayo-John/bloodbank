8aed196416ce3b7f86109d3847933fbfd5512e7f
Olatayo-John <55631654+Olatayo-John@users.noreply.github.com>
2021-06-17 15:23:43 +0530

    changes
    try reading the comments

--- controller/signup.controller.php
+++ controller/signup.controller.php
@@ -1,7 +1,32 @@
 <?php
-include('../connection/connection.php');
 
-function createUserTableIfNotExist($connection)
+if ($_SERVER['REQUEST_METHOD'] == "POST") { //validate request method because its ajax
+
+    include('../connection/connection.php');
+
+    //dont check for table exist, it will confuse and slow ure code..just create table manually and run insertuser function
+    //error will come when u try to insert data inside the table
+    // createUserTableIfNotExist($conn);
+
+    insertUser($conn);
+} else {
+    $data['status'] = 'error';
+    $data['msg'] = 'not safe';
+}
+
+//dont call any function outside the request method.... i.e line 9 and 11, for security reasons
+// createUserTableIfNotExist($conn); outside request method
+// insertUser($conn);
+
+function test_data($data)
+{
+    $data = trim($data);
+    $data = stripcslashes($data);
+    $data = htmlentities($data);
+    return $data;
+}
+
+function createUserTableIfNotExist($conn)
 {
     // sql to create table
     $sql = "CREATE TABLE users (
@@ -15,33 +40,41 @@ function createUserTableIfNotExist($connection)
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     )";
 
-    if (mysqli_query($connection, $sql) === TRUE) {
-        echo "Table users created successfully";
+    if (mysqli_query($conn, $sql) === TRUE) {
+        // ure using ajax, dont use echo
+        // echo "Table users created successfully";
+        //at this point, there's error if the table already exist
+        $data['status'] = 'success';
+        $data['msg'] = 'Table users created successfully';
     } else {
-        echo "Error creating table: " . mysqli_connect_error($connection);
+        // echo "Error creating table: " . mysqli_connect_error($connection);
+        $data['status'] = 'error';
+        $data['msg'] = "Error creating table: " . mysqli_connect_error($conn);
     }
-}
 
-// createUserTableIfNotExist($conn);
-insertUser($conn);
+    echo json_encode($data);
+}
 
 function insertUser($conn)
 {
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-            echo $_POST['email'];
+    $first_name = test_data($_POST['fname_v']);
+    $last_name = test_data($_POST['lname_v']);
+    $email = test_data($_POST['email_v']);
+    $password = test_data($_POST['pwd_v']);
+    $gender = test_data($_POST['gender_v']);
+
+    if (!empty($first_name) && !empty($last_name)) { //check if all the required values are not empty
+        if (!filter_var($_POST['email_v'], FILTER_VALIDATE_EMAIL)) {
+            //again, dont use echo, its ajax...echo works but it wount show unless its in the neworks tab directly
+            // echo $_POST['email'];
+
             $data['success'] = false;
             $data['status'] = "invalid";
             $data['message'] = "Invalid Email";
-            echo json_encode($data);
+
+            //no need for multiple echo json_encode($data), just put it at end of function
+
         } else {
-
-            $first_name = test_data($_POST['first_name']);
-            $last_name = test_data($_POST['last_name']);
-            $email = test_data($_POST['email']);
-            $password = test_data($_POST['password']);
-            $gender = test_data($_POST['gender']);
-
             $hash = password_hash($password, PASSWORD_DEFAULT);
             echo $hash;
             $query = "INSERT INTO users (first_name, last_name, email, password ,gender) VALUES ('" . mysqli_real_escape_string($conn, $first_name) . "','" . mysqli_real_escape_string($conn, $last_name) . "','" . mysqli_real_escape_string($conn, $email) . "','" . mysqli_real_escape_string($conn, $hash) . "','" . mysqli_real_escape_string($conn, $gender) . "');";
@@ -51,26 +84,20 @@ function insertUser($conn)
                 $data['success'] = false;
                 $data['status'] = "invalid";
                 $data['message'] = "Error while creating user";
-                echo json_encode($data);
+                // echo json_encode($data);
             } else {
                 $data['success'] = true;
                 $data['status'] = "success";
                 $data['message'] = "User created successfully";
-                echo json_encode($data);
+                // echo json_encode($data);
             }
         }
     } else {
         $data['success'] = false;
         $data['status'] = "invalid";
         $data['message'] = "Error Occured";
-        echo json_encode($data);
+        // echo json_encode($data);
     }
-}
 
-function test_data($data)
-{
-    $data = trim($data);
-    $data = stripcslashes($data);
-    $data = htmlentities($data);
-    return $data;
+    echo json_encode($data);
 }
